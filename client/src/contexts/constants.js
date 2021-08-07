00e8d11687fe5a1ad5c9529dd592bcd119fee70c
Vanquy1306 <59086073+Vanquy1306@users.noreply.github.com>
2021-08-07 10:24:17 +0700

    Update constants.js

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
-		? 'http://localhost:5000/api':'https://musing-lamport-dec482.netlify.app/api'
+		? 'http://localhost:5000/api':'https://610dfc14fd210e601564df54--musing-lamport-dec482.netlify.app/api'
 
         export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
         export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS'
